6d7e2a86653cea5cbb1347da6083ae81456ef7d6
joselynvcr <n00057871@upn.pe>
2021-02-03 21:34:35 -0500

    librerias

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include "Nuevo.h"
 using namespace std;
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
 	cout<<"New create";
-	return 0;
+	cout<<suma();
+		return 0;
 }
